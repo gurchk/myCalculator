8e7cb5445ff199732dcde935b173afedcdf4c874
Gustav <gustav.karlstrom96@hotmail.com>
2018-03-27 17:49:16 +0200

    Initial commit

---
+++ src/router/index.js
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Calculator from '@/components/calculator.vue';
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'Calculator',
+      component: HelloWorld
+    }
+  ]
+})
